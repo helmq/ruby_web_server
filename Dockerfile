8a93dbcdf177bdf154ee0b4de0a26b5170bb4344
Viktor Kuli <viktor.kuli@dualbootpartners.com>
2020-10-03 15:57:07 +0400

    add content-type

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM ruby:2.7.1-alpine3.12
 RUN apk update && apk upgrade
 RUN apk add bash
 RUN apk add curl-dev ruby-dev build-base git curl ruby-json openssl apache2-utils sqlite-libs sqlite sqlite-dev
+RUN gem install mime-types
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
--- multiple_threads.rb
+++ multiple_threads.rb
@@ -2,36 +2,48 @@
 # head -c 100000 /dev/urandom > section_one/ostechnix_big.txt
 
 require 'socket'
+require 'pathname'
+require 'mime-types'
 require './lib/response'
 require './lib/request'
 MAX_EOL = 2
+
+ROOT_PATH = '/'
+DEFAULT_CONTENT_TYPE = 'plain/text'
 
 socket = TCPServer.new(ENV['HOST'], ENV['PORT'])
 
 def file_path(request)
   dirname = Dir.getwd
 
-  "#{dirname}#{request.path}"
+  Pathname.new("#{dirname}#{request.path}")
 end
 
-def create_output(filepath)
+def read_file(filepath)
   file_content = nil
 
   File.open(filepath, "r") do |file|
     file_content = file.read
   end
 
-  file_content.nil? ? "Hello world" : file_content
+  file_content
 end
 
 def handle_request(request_text, client)
   request = Request.new(request_text)
   puts "#{client.peeraddr[3]} #{request.path}"
 
-  filepath = file_path(request)
-  output = create_output(filepath)
+  content_type = DEFAULT_CONTENT_TYPE
 
-  response = Response.new(code: 200, data: output)
+  output = if request.path == ROOT_PATH
+     "Hello world"
+  else
+    filepath = file_path(request)
+    content_type = MIME::Types.type_for(File.extname(filepath)).first
+    read_file(filepath)
+  end
+
+  response = Response.new(code: 200, data: output, headers: ["Content-Type: #{content_type}"])
   response.send(client)
 
   client.shutdown
@@ -54,6 +66,13 @@ def handle_connection(client)
       break
     end
   end
+rescue Errno::EPERM => e
+  puts "Error: #{e}"
+
+  response = Response.new(code: 403, data: "Permission Denied")
+  response.send(client)
+
+  client.close
 rescue Errno::ENOENT => e
   puts "Error: #{e}"
 
